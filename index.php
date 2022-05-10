36b4e8c9931c5416c2fdc83a0e817486a7885208
jvtrindade <jv.trindadesilva@gmail.com>
2022-05-09 21:59:58 -0300

    Tentei melhorar, tem que ver melhor

--- index.php
+++ index.php
@@ -6,11 +6,19 @@
         $ingredientes = [];
         $cardapios = [];
         $nutricionistas = [];
-        foreach($database->query("SELECT * FROM itens")
+        foreach($database->query("SELECT *,
+        (SELECT SUM(ingredientes.calorias) as calorias_item FROM ingredientes
+        /*INNER JOIN
+        itens_ingredientes ON itens_ingredientes.id_item = itens.id
+        INNER JOIN
+        ingredientes ON ingredientes.id = itens_ingredientes.id_ingrediente*/
+        WHERE ingredientes.id = itens.id)
+        FROM itens")
         as $item){
             $itens[] = [
                 "id" => $item["id"],
-                "descricao" => $item["descricao"]
+                "descricao" => $item["descricao"],
+                "calorias_item" => $item["calorias_item"]
             ];
         }
         foreach($database->query("SELECT * FROM ingredientes")
@@ -36,6 +44,10 @@
                 "nome" => $nutricionista["nome"]
             ];
         }
+        print json_encode($nutricionistas);
+        print json_encode($cardapios);
+        print json_encode($ingredientes);
+        print json_encode($itens);
     }
     catch(PDOException $e){
         die($e->getMessage());
