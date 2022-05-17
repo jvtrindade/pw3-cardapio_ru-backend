faab0c7cec3d2ee62f3606a99b007f60327563cd
Joao Silva <joao.210105@bento.local>
2022-05-17 09:58:56 -0300

    correção de erros

--- autenticar.php
+++ autenticar.php
@@ -4,13 +4,18 @@
     $senha = $_POST["senha"];
     $database = new PDO("mysql:localhost=host;dbname=ru", "root", "root");
 
-    foreach($database->query("SELECT * FROM usuarios") as $dados){
-        if ($usuario == $dados["email"] && $senha == $dados["senha"]){
-            header("Location: ../privado/index.php");
-        }
-        else{
-            die("Usuário ou Senha incorretos");
-        }
-    }
+    $consulta = $database->prepare("SELECT * FROM usuarios WHERE email=:email and senha =:senha LIMIT 1");
+    $consulta->execute([":email" => $usuario, ':senha' => $senha]);
+    $consulta->setFetchMode(PDO::FETCH_CLASS, "Usuario");
+    die(var_dump($consulta->fetch()));
+
+    // foreach($database->prepare("SELECT * FROM usuarios WHERE email = ? and senha =  ?") as $dados){
+    //     if ($usuario == $dados["email"] && $senha == $dados["senha"]){
+    //         header("Location: ../privado/index.php");
+    //     }
+    //     else{
+    //         die("Usuário ou Senha incorretos");
+    //     }
+    // }
 
 ?>
