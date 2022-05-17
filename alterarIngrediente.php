1d3cc674bbecff0dd289ff6dc13ca4a336fa9d47
Julia Conzatti <julia.140904@bento.local>
2022-05-17 10:04:43 -0300

    agr sim alterar ingredientes

--- alterarIngrediente.php
+++ alterarIngrediente.php
@@ -1,2 +1,16 @@
 <?php
+
+require "classIngrediente.php";
+
+$id = $_GET["id"];
+
+$ing = Ingrediente::findbyPk($id);
+if(!$p){
+    throw new Exception("Ingrediente não encontrado!");
+}
+$n->setId($_POST["id"]);
+$n->setDescricao($_POST["descricao"]);
+$n->alterar();
+print $n;
+
 ?>
