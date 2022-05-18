e4294f5ed45816a3fe21c267355bc580f95bb809
João Vitor Angelo Pereira <jvangelopereira@gmail.com>
2022-05-18 09:15:29 -0300

    conserto de erros

--- data.sql
+++ data.sql
@@ -1,5 +1,5 @@
-INSERT INTO usuarios VALUES ("Alyssa Dillenburg Michalski", "alyssadillenburgm@gmail.com", "grupoajjjj")
-INSERT INTO usuarios VALUES ("João Victor Freitas Batistela Trindade da Silva", "jv.trindadesilva@gmail.com", "grupoajjjj")
-INSERT INTO usuarios VALUES ("João Vitor Angelo Pereira", "jvangelopereira@gmail.com", "grupoajjjj")
-INSERT INTO usuarios VALUES ("Juan Álvaro Córdova", "juanalvaro067@gmail.com", "grupoajjjj")
-INSERT INTO usuarios VALUES ("Júlia Conzatti", "juliarconzatti@gmail.com", "grupoajjjj")
+INSERT INTO usuarios (nome, email, senha) VALUES ("Alyssa Dillenburg Michalski", "alyssadillenburgm@gmail.com", "grupoajjjj");
+INSERT INTO usuarios (nome, email, senha) VALUES ("João Victor Freitas Batistela Trindade da Silva", "jv.trindadesilva@gmail.com", "grupoajjjj");
+INSERT INTO usuarios (nome, email, senha) VALUES ("João Vitor Angelo Pereira", "jvangelopereira@gmail.com", "grupoajjjj");
+INSERT INTO usuarios (nome, email, senha) VALUES ("Juan Álvaro Córdova", "juanalvaro067@gmail.com", "grupoajjjj");
+INSERT INTO usuarios (nome, email, senha) VALUES ("Júlia Conzatti", "juliarconzatti@gmail.com", "grupoajjjj");
