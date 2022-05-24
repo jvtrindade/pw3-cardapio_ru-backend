46f3a04c55ea1cbac5aa083c97290aeb2976dd1b
Joao Silva <joao.210105@bento.local>
2022-05-24 09:49:00 -0300

    Autenticar

--- autenticar.php
+++ autenticar.php
@@ -1,45 +1,25 @@
 <?php
 
-    /*$usuario = $_POST["email"];
-    $senha = $_POST["senha"];
-    $database = new PDO("mysql:localhost=host;dbname=ru", "root", "root");
-
-    $consulta = $database->prepare("SELECT * FROM usuarios WHERE email=:email and senha =:senha LIMIT 1");
-    $consulta->execute([":email" => $usuario, ':senha' => $senha]);
-    $consulta->setFetchMode(PDO::FETCH_CLASS, "Usuario");
-    $consulta->fetch();
-
-    foreach($database->prepare("SELECT * FROM usuarios WHERE email = :email and senha =  :senha LIMIT 1") as $dados){
-        if ($usuario == $dados["email"] && $senha == $dados["senha"]){
-            header("Location: ../frontend/privado/index.php");
+    if(session_status() !== PHP_SESSION_NONE){
+        $usuario = $_POST["email"];
+        $senha = $_POST["senha"];
+        $database = new PDO("mysql:localhost=host;dbname=ru", "aluno", "aluno");
+    
+        $consulta = $database->prepare("SELECT * FROM usuarios WHERE email=:email and senha =:senha");
+        $consulta->execute([":email" => $usuario, ':senha' => $senha]);
+        $consulta->setFetchMode(PDO::FETCH_CLASS, "Usuario");
+        $dados = $consulta->fetch();
+        if($dados != false){
+            session_start();
+            header("Location: autenticar.php");
         }
         else{
-            die("Usuário ou Senha incorretos");
+            header("HTTP/1.0 404 Not Found");
+            die("Usuário não encontrado!");
         }
-    }*/
-
-    if(session_start() == false){
-
-    $usuario = $_POST["email"];
-    $senha = $_POST["senha"];
-    $database = new PDO("mysql:localhost=host;dbname=ru", "aluno", "aluno");
- 
-    $consulta = $database->prepare("SELECT * FROM usuarios WHERE email=:email and senha =:senha");
-    $consulta->execute([":email" => $usuario, ':senha' => $senha]);
-    $consulta->setFetchMode(PDO::FETCH_CLASS, "Usuario");
-    $dados = $consulta->fetch();
-    if($dados != false){
-        session_start();
-        header("Location: autenticar.php");
     }
     else{
-        header("HTTP/1.0 404 Not Found");
-        die("Usuário não encontrado!");
-    }
-    }
-
-    else{
-        header("Location: ../frontend/privado/index.html");
+        header ("Location: ../frontend/privado/index.html");
     }
 
 ?>
