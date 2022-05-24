5e486c568ceb34544d5ab6e1bd55a889aaf121af
Joao Silva <joao.210105@bento.local>
2022-05-24 09:04:04 -0300

    Melhorando autenticacao

--- autenticar.php
+++ autenticar.php
@@ -20,12 +20,11 @@
 
     $usuario = $_POST["email"];
     $senha = $_POST["senha"];
-    $database = new PDO("mysql:localhost=host;dbname=ru", "root", "root");
+    $database = new PDO("mysql:localhost=host;dbname=ru", "root", "");
  
     $consulta = $database->prepare("SELECT * FROM usuarios WHERE email=:email and senha =:senha LIMIT 1");
     $consulta->execute([":email" => $usuario, ':senha' => $senha]);
     $consulta->setFetchMode(PDO::FETCH_CLASS, "Usuario");
-    die(var_dump($consulta->fetch()));
     foreach ($consulta as $c){
         if ($usuario == $c["email"] && $senha == $c["senha"]){
             session_start();
