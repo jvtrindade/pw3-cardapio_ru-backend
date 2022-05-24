eb4581de21324e4036066b6464a0f89c9fe4a834
Joao Silva <joao.210105@bento.local>
2022-05-24 09:28:22 -0300

    Autenticação sem sessão

--- autenticar.php
+++ autenticar.php
@@ -20,19 +20,19 @@
 
     $usuario = $_POST["email"];
     $senha = $_POST["senha"];
-    $database = new PDO("mysql:localhost=host;dbname=ru", "root", "");
+    $database = new PDO("mysql:localhost=host;dbname=ru", "aluno", "aluno");
  
-    $consulta = $database->prepare("SELECT * FROM usuarios WHERE email=:email and senha =:senha LIMIT 1");
+    $consulta = $database->prepare("SELECT * FROM usuarios WHERE email=:email and senha =:senha");
     $consulta->execute([":email" => $usuario, ':senha' => $senha]);
     $consulta->setFetchMode(PDO::FETCH_CLASS, "Usuario");
-    foreach ($consulta as $c){
-        if ($usuario == $c["email"] && $senha == $c["senha"]){
-            session_start();
-            header("Location: ../frontend/privado/index.php");
-        }
-        else{
-            die("Usuário ou Senha incorretos");
-        }
+    $dados = $consulta->fetch();
+    if($dados != false){
+        session_start();
+        header("Location: ../frontend/privado/index.html");
+    }
+    else{
+        header("HTTP/1.0 404 Not Found");
+        die("Usuário não encontrado!");
     }
 
 ?>
