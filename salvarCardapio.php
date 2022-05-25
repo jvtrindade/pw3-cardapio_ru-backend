a48bb0f856366fe71840bb2e51726a3c550960e0
Julia Conzatti <julia.140904@bento.local>
2022-05-25 10:00:10 -0300

    b

--- salvarCardapio.php
+++ salvarCardapio.php
@@ -1,10 +1,11 @@
 <?php
-require "class.Itens.php";
+require "lib/classCardapio.php";
 
 try {
-    $p = new Itens();
-    $p->setDescricao($_POST['descricao']);
-    $p->inserir();
+    $p = new Cardapio();
+    $p->setDia($_POST['dia']);
+    $p->setTipo($_POST['tipo']);
+    $p->inserirCardapio();
     print $p;
 }catch(Exception $e){
     print json_encode([
