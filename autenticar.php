9e18223ef6cf9d32a708136841dd771d689d7768
Joao Silva <joao.210105@bento.local>
2022-05-25 09:43:50 -0300

    database

--- autenticar.php
+++ autenticar.php
@@ -28,6 +28,7 @@
     use Firebase\JWT\Key;
     $usuario = $_POST['email'];
     $senha = $_POST['senha'];
+    $database = new PDO("mysql:localhost=host;dbname=ru", "aluno", "aluno");
     try {
         
         $consulta = $database->prepare("SELECT id, nome FROM usuarios WHERE email=:email and senha =:senha");
