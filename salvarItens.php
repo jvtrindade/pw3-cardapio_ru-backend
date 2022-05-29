b8af78f74a91c2abf610fbf83fc8d556995d2a84
jvtrindade <jv.trindadesilva@gmail.com>
2022-05-29 18:50:48 -0300

    Temos q mudar o salvarItens

--- salvarItens.php
+++ salvarItens.php
@@ -3,7 +3,7 @@ require "class.Itens.php";
 
 try {
     $p = new Itens();
-    $p->setDescricao($_POST['descricao']);
+    $p->setDescricao($_POST['descricao']); // isso aqui tem que ser diferente pq não vai ter um campo de descrição pro item
     $p->inserir();
     print $p;
 }catch(Exception $e){
