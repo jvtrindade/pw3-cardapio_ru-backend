d7283af150089f42bdccb809629c78809c261456
Joao Silva <joao.210105@bento.local>
2022-05-31 09:50:36 -0300

    Pesquisa

--- pesquisar.php
+++ pesquisar.php
@@ -1,17 +1,29 @@
 <?php
 
-    $refeicao = $_POST["refeicao"];
-    $dia = $_POST["data"];
-    $item = $_POST["item"];
+    $refeicao = //$_POST["refeicao"];
+    $dia = //$_POST["data"];
+    $item = "Tapioca"; //$_POST["item"];
     $database = new PDO("mysql:host=localhost;dbname=ru", "aluno", "aluno");
-    $consultaItem = $database->prepare("SELECT * FROM itens WHERE descricao=:descricao");
-    $consultaItem = $database->execute([":descricao" => $item]);
-    $consultaData = $database->prepare("SELECT * FROM cardapio WHERE dia=:dia");
-    $consultaData = $database->execute([":dia" => $dia]);
-    $consultaRefeicao = $database->prepare("SELECT * FROM cardapio WHERE tipo=:refeicao");
-    $consultaRefeicao = $database->execute([":refeicao" => $refeicao]);
+    $consultaItem = $database->prepare("SELECT descricao FROM itens WHERE descricao=:descricao");
+    $consultaItem->execute([":descricao" => $item]);
+    $consultaItem;//tem q fazer o fetch
+    $dadoItem = $consultaItem->fetch();
 
-    print json_encode($consulta)
+    $consultaData = $database->prepare("SELECT dia FROM cardapio WHERE dia=:dia");
+    $consultaData->execute([":dia" => $dia]);
+    $consultaData;//fetch
+    $dadoData = $consultaData->fetch();
+
+    $consultaRefeicao = $database->prepare("SELECT tipo FROM cardapio WHERE tipo=:refeicao");
+    $consultaRefeicao->execute([":refeicao" => $refeicao]);
+    $consultaRefeicao;//fetch
+    $dadoRefeicao = $consultaRefeicao->fetch();
+
+    var_dump($consultaRefeicao);
+
+    print json_encode($dadoItem);
+    print json_encode($dadoData);
+    print json_encode($dadoRefeicao);
 
 
 ?>
