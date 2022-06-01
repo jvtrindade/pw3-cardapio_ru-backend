3bb7fb51628cfc5cb7df7243045c410093b18e4b
Joao Silva <joao.210105@bento.local>
2022-06-01 08:55:08 -0300

    Verificar se item já existe

--- salvarItens.php
+++ salvarItens.php
@@ -2,6 +2,15 @@
 require "class.Itens.php";
 
 try {
+    foreach($database->query("SELECT * FROM itens") as $itens){
+        $itens[] = [
+            "id" => $itens["id"],
+            "descricao" => $itens["descricao"],
+        ];
+        if ($_POST["descricao"] = $itens["descricao"]){
+            print json_encode("Item já cadastrado");
+        }
+    }
     $p = new Itens();
     $p->setDescricao($_POST['descricao']); // isso aqui tem que ser diferente pq não vai ter um campo de descrição pro item
     $p->inserir();
