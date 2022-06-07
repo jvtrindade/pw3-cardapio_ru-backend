290ee238812c0ec1e788521d29a04cb4c0c6b13b
Julia Conzatti <julia.140904@bento.local>
2022-06-07 10:03:28 -0300

    oig

--- salvarNutricionista.php
+++ salvarNutricionista.php
@@ -3,14 +3,14 @@
     require "classNutricionista.php";
 
     try{
-        foreach($database->query("SELECT * FROM nutricionistas") as $nutricionista){
-            $nutricionista[] = [
-                "crn" => $nutricionista["crn"],
-            ];
-            if ($_POST["crn"] = $nutricionistas["crn"]){
-                print json_encode("Nutricionista já cadastrado(a)");
-            }
-        }
+        // foreach($database->query("SELECT * FROM nutricionistas") as $nutricionista){
+        //     $nutricionista[] = [
+        //         "crn" => $nutricionista["crn"],
+        //     ];
+        //     if ($_POST["crn"] = $nutricionistas["crn"]){
+        //         print json_encode("Nutricionista já cadastrado(a)");
+        //     }
+        // }
         $n = new Nutricionista();
         $n->setCRN($_POST["crn"]);
         $n->setNome($_POST["nome"]);
