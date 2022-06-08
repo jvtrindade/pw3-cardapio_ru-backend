d9159f85f191755471085e4699eed396b73f6609
jvtrindade <jv.trindadesilva@gmail.com>
2022-06-07 21:48:14 -0300

    banco

--- schema/data.sql
+++ schema/data.sql
@@ -3,4 +3,6 @@ INSERT INTO usuarios (nome, email, senha) VALUES ("João Victor Freitas Batistel
 INSERT INTO usuarios (nome, email, senha) VALUES ("João Vitor Angelo Pereira", "jvangelopereira@gmail.com", "grupoajjjj");
 INSERT INTO usuarios (nome, email, senha) VALUES ("Juan Álvaro Córdova", "juanalvaro067@gmail.com", "grupoajjjj");
 INSERT INTO usuarios (nome, email, senha) VALUES ("Júlia Conzatti", "juliarconzatti@gmail.com", "grupoajjjj");
-INSERT INTO ingredientes (descricao, calorias) VALUES ("Tomate", "50")
+INSERT INTO ingredientes (descricao, calorias) VALUES ("Tomate", "50");
+INSERT INTO nutricionistas (crn, nome) VALUES ("12345678", "Rejane");
+INSERT INTO cardapios (dia, tipo, crn_nutricionista) VALUES (2020-10-10, 1, "12345678");
